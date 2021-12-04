41eae826975554d9918b9c797b50d6d639a322ce
mqthang_18sh <maiquocthangt@gmail.com>
2021-12-04 20:56:05 +0700

    Add catch error

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+  try {
   var textHTML = '<div>'.concat(header, container, data, footer, '</div>');
-
-
   console.log(textHTML)
   var app = new Vue({
     el: "#app",
     template: textHTML
   })
+} catch (err) {
+  console.log('Error');
+}
+  
