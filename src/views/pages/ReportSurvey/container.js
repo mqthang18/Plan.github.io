37cceb5ac5e8ca4bdfc97999ac15b28899b77ab4
mqthang_18sh <maiquocthangt@gmail.com>
2022-01-26 16:38:48 +0700

    Update

--- src/views/pages/ReportSurvey/container.js
+++ src/views/pages/ReportSurvey/container.js
@@ -3,7 +3,7 @@ const container = `
         <h3>Nội dung -  Bản scan phiếu phỏng vấn: <a href="https://drive.google.com/drive/folders/1seI3cR4vWnbsJ3kWray9bDZo3_Sb-pp1?usp=sharing" target="_blank">Link</a></h3>
         <ul>
             <li>
-                <a href="#" v-on:click="scrollToBottom($event,'HQ')">Đánh giá chất lượng sinh cảnh</a>
+                <a href="#" v-on:click="scrollToBottom($event,'HQ')">Đánh giá chất lượng sinh cảnh bằng mô hình Habitat Quality</a>
                 <ol>
                     <li>
                         <a href="#" v-on:click="scrollToBottom($event,'HQ1')">Mô tả diễn biến lớp phủ quá khứ đến nay</a>
@@ -18,32 +18,26 @@ const container = `
                         <a href="#" v-on:click="scrollToBottom($event,'HQ4')">Mô tả những thay đổi của sinh cảnh trong tương lai</a>
                     </li>
                     <li>
-                        <a href="#" v-on:click="scrollToBottom($event,'HQ5')">Biểu diễn khu vực phân bố</a>
+                        <a href="#" v-on:click="scrollToBottom($event,'HQ5')">Biểu diễn khu vực phân bố các nhân tố và ước lượng khoảng cách tác động tối đa của nhân tố - <span style="color:red;">dữ liệu phải có cho mô hình Habitat quality (INVEST)</span></a>
                     </li>
                     <li>
-                        <a href="#" v-on:click="scrollToBottom($event,'HQ6')">Mô tả những thay đổi sinh cảnh trong tương lai</a>
+                        <a href="#" v-on:click="scrollToBottom($event,'HQ6')">Mô tả thay đổi về mức độ tác động của nhân tố qua không gian - <span style="color:red;">dữ liệu phải có cho mô hình Habitat quality (INVEST)</span></a>
                     </li>
                     <li>
-                        <a href="#" v-on:click="scrollToBottom($event,'HQ7')">Biểu diễn khu vực phân bố</a>
+                        <a href="#" v-on:click="scrollToBottom($event,'HQ7')">Biểu diễn sự chuyển dịch trong tương lai khu vực phân bố tác nhân gây suy thoái chất lượng sinh cảnh</a>
                     </li>
                     <li>
-                        <a href="#" v-on:click="scrollToBottom($event,'HQ8')">Mô tả thay đổi về mức độ tác động của nhân tố qua không gian</a>
+                        <a href="#" v-on:click="scrollToBottom($event,'HQ8')">
+                        Trọng số giữa các cặp tác động - <span style="color:red;">dữ liệu phải có cho mô hình Habitat quality (INVEST)</span>
+                        </a>
                     </li>
                     <li>
-                        <a href="#" v-on:click="scrollToBottom($event,'HQ9')">Biểu diễn sự chuyển dịch trong tương lai khu vực phân bố tác nhân gây suy thoái chất lượng sinh cảnh</a>
+                        <a href="#" v-on:click="scrollToBottom($event,'HQ9')">
+                        Mức độ tác động tiêu cực lên sinh cảnh của các nhân tố tác động - <span style="color:red;">dữ liệu phải có cho mô hình Habitat quality (INVEST)</span>
+                        </a>
                     </li>
                     <li>
                         <a href="#" v-on:click="scrollToBottom($event,'HQ10')">
-                        Trọng số giữa các cặp tác động
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" v-on:click="scrollToBottom($event,'HQ11')">
-                        Mức độ tác động tiêu cực lên sinh cảnh của các nhân tố tác động
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#" v-on:click="scrollToBottom($event,'HQ12')">
                         Mức độ tác động tích cực lên sính cảnh                             
                         </a>
                     </li>
--- src/views/pages/ReportSurvey/data.js
+++ src/views/pages/ReportSurvey/data.js
@@ -1,6 +1,6 @@
 const data = `
     <div id="HQ" style="min-height: 300px;">
-        <h2>Đánh giá chất lượng sinh cảnh - tóm tắt kết quả: <a href="https://docs.google.com/forms/d/1GA-fs4wv9yP0p14KHl0WzkyEN2wRSSS53XJMa4LnPmA/viewanalytics" target="_blank">Link</a></h2>
+        <h2>Đánh giá chất lượng sinh cảnh bằng mô hình Habitat Quality - tóm tắt kết quả: <a href="https://docs.google.com/forms/d/1GA-fs4wv9yP0p14KHl0WzkyEN2wRSSS53XJMa4LnPmA/viewanalytics" target="_blank">Link</a></h2>
         <ol>
             <li id="HQ1">
                 <button type="button" id="collapsible" class="collapsible">Mô tả diễn biến lớp phủ quá khứ đến nay</button>
@@ -79,8 +79,10 @@ const data = `
                 </div>
             </li>
             <li id="HQ5">
-                <button type="button" id="collapsible" class="collapsible">Biểu diễn khu vực phân bố của các nhân tố</button>
+                <button type="button" id="collapsible" class="collapsible">Biểu diễn khu vực phân bố của các nhân tố ước lượng khoảng cách tác động tối đa của nhân tố - <span style="color:red;">dữ liệu phải có cho mô hình Habitat quality (INVEST)</span></button>
                 <div id="content" class="content" style="width: 98.5%">
+                    <p><span style="color:red; font-weight: 600;">Đã đạt được</span>: Qua phỏng vấn, nhóm đã đạt được bản đồ tương đối về vị trí phân bố của các nhân tố gây suy thoái chất lượng sinh cảnh tại khu vực nghiên cứu.</p>
+                    <p><span style="color:red; font-weight: 600;">Chưa đạt được</span>: <span style="text-decoration: underline;">Khoảng cách tác động tối đa (MAX DISTANCE)</span> của nhân tố các nhân tố gây suy thoái chất lượng sinh cảnh được xác định.</p>
                     <ul>
                         <li>
                             <h5>Sóc Trăng</h5>
@@ -100,46 +102,19 @@ const data = `
                     
                 </div>
             </li>
+       
             <li id="HQ6">
-                <button type="button" id="collapsible" class="collapsible">Mô tả những đổi sinh cảnh trong tương lai</button>
+                <button type="button" id="collapsible" class="collapsible">Mô tả thay đổi về mức độ tác động của nhân tố qua không gian - <span style="color:red;">dữ liệu phải có cho mô hình Habitat quality (INVEST)</span></button>
                 <div id="content" class="content" style="width: 98.5%">
+                    <p>Trong đợt phỏng vấn vừa rồi hầu như <span style="color:red;">không đạt được</span> dữ liệu liên quan tới mô tả <span style="color:red;">sự biến đổi mức độ tác động của nhân tố</span> qua không gian theo các kiểu "exponential" hay "linear" ở cả 3 tỉnh.</p>
                     <ul>
-                        <li>
-                            <h5>Sóc Trăng<h5>
-                            <iframe style="width: 100%; height: 320px;" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSVIrpQmW0JWTuRDy3CQ6S8T38-reoZRLy6SNm0Gz_Pn8eAGDE-G97rl6jGUnUoXdlcymqUGPxmz6Wp/pubhtml?gid=1011375499&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
-                        </li>
-                        <li>
-                            <h5>Cà Mau</h5>
-                            <iframe style="width: 100%; height: 320px;" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTWgrGXor2Kkpl9b9BbOS7LJS-pVxv3XvdQk7GAWHYvw3FCz8CkD-KIkot0dfl2unbMOTvbJQGI43iw/pubhtml?gid=826969243&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
-                        </li>
-                        <li>
-                            <h5>Kiên Giang</h5>
-                            <iframe style="width: 100%; height: 320px;" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRfRWl5FLqagothETucUplXafsKacrYYtJVarJ0gffl1MvFXlkA2m7ZtkR58-pbDAl7WvM4byP2_qsy/pubhtml?gid=85697556&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
-                        </li>
+                        <li><h5>Sóc Trăng</h5></li>
+                        <li><h5>Cà Mau</h5></li>
+                        <li><h5>Kiên Giang</h5></li>
                     </ul>
                 </div>
             </li>
             <li id="HQ7">
-                <button type="button" id="collapsible" class="collapsible">Biểu diễn chuyển dịch trong tương lai khu vực phân bố tác nhân gây suy thoái chất lượng sinh cảnh</button>
-                <div id="content" class="content" style="width: 98.5%">
-                    <ul>
-                        <li><h5>Sóc Trăng</h5></li>
-                        <li><h5>Cà Mau</h5></li>
-                        <li><h5>Kiên Giang</h5></li>
-                    </ul>
-                </div>
-            </li>
-            <li id="HQ8">
-                <button type="button" id="collapsible" class="collapsible">Mô tả thay đổi về mức độ tác động của nhân tố qua không gia</button>
-                <div id="content" class="content" style="width: 98.5%">
-                    <ul>
-                        <li><h5>Sóc Trăng</h5></li>
-                        <li><h5>Cà Mau</h5></li>
-                        <li><h5>Kiên Giang</h5></li>
-                    </ul>
-                </div>
-            </li>
-            <li id="HQ9">
                 <button type="button" id="collapsible" class="collapsible">Biểu diễn sự chuyển dịch trong tương lai khu vực phân bố tác nhân gây suy thoái chất lượng sinh cảnh</button>
                 <div id="content" class="content" style="width: 98.5%">
                     <ul>
@@ -149,9 +124,11 @@ const data = `
                     </ul>
                 </div>
             </li>
-            <li id="HQ10">
-                <button type="button" id="collapsible" class="collapsible">Trọng số giữa các cặp tác động </button>
+            <li id="HQ8">
+                <button type="button" id="collapsible" class="collapsible">Trọng số giữa các cặp tác động - <span style="color:red;">dữ liệu phải có cho mô hình Habitat quality (INVEST)</span></button>
                 <div id="content" class="content" style="width: 98.5%">
+                    <p><span style="color:red; font-weight:600;">Đã làm được</span>: Đối với khu vực <span style="color:red">Sóc Trăng và Cà Mau</span>, dữ liệu đã đáp ứng được</p>
+                    <p><span style="color:red; font-weight:600;">Chưa làm được</span>: Đối với khu vực Kiên Giang, hiện tại chưa đủ dữ liệu</p>
                     <ul>
                         <li>
                             <h5>Sóc Trăng</h5>
@@ -165,9 +142,11 @@ const data = `
                     </ul>
                 </div>
             </li>
-            <li id="HQ11">
-                <button type="button" id="collapsible" class="collapsible">Mức độ tác động tiêu cực lên sinh cảnh của các nhân tố tác động</button>
+            <li id="HQ9">
+                <button type="button" id="collapsible" class="collapsible">Mức độ tác động tiêu cực lên sinh cảnh của các nhân tố tác động - <span style="color:red;">dữ liệu phải có cho mô hình Habitat quality (INVEST)</span></button>
                 <div id="content" class="content" style="width: 98.5%">
+                    <p><span style="color:red; font-weight:600;">Đã làm được</span>: Đối với khu vực <span style="color:red">Sóc Trăng và Cà Mau</span>, dữ liệu đã đáp ứng được</p>
+                    <p><span style="color:red; font-weight:600;">Chưa làm được</span>: Đối với khu vực Kiên Giang, hiện tại chưa đủ dữ liệu</p>
                     <ul>
                         <li>
                             <h5>Sóc Trăng</h5>
@@ -187,7 +166,7 @@ const data = `
                     </ul>
                 </div>
             </li>
-            <li id="HQ12">
+            <li id="HQ10">
                 <button type="button" id="collapsible" class="collapsible">Mức độ tác động tích cực lên sinh cảnh</button>
                 <div id="content" class="content" style="width: 98.5%">
                     <ul>
