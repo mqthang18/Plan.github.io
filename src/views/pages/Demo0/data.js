bc92b04b80291489b69f5ece958523fc72a9a22c
mqthang_18sh <maiquocthangt@gmail.com>
2022-03-02 11:16:28 +0700

    Update

--- src/views/pages/Demo0/data.js
+++ src/views/pages/Demo0/data.js
@@ -121,8 +121,8 @@ const data = `
     <div id="ManuscriptHRA">
         <button id="collapsible" class="collapsible" style="text-decoration: underline;">4. Manuscript HRA - đánh giá rủi ro sinh cảnh</button>
         <div id="content" class="content" style="width: 100vw; max-width: 100%;">
-            <p><a href="https://docs.google.com/document/d/11DWifpJaLBWrLeh8Ahu9iFuKGsZVfz0H/edit?usp=sharing&ouid=103377131320564328842&rtpof=true&sd=true" target="_blank">Link manuscript đánh giá rủi ro sinh cảnh</a></p>
-            <p style="text-align: center;"><iframe style="width: 90vw; height: 80vh;" src="https://docs.google.com/document/d/e/2PACX-1vQdyuhRc5L85h1sKIZOrmeEg2-dsoLn2V6y3poYcoNkZogJK0Xw-Q2K-sVywqhkHg/pub?embedded=true"></iframe></p>
+            <p><a href="https://docs.google.com/document/d/12N_Q2-7D_m1JR06PlWBW8BPa_DDi3qob/edit?usp=sharing&ouid=103377131320564328842&rtpof=true&sd=true" target="_blank">Link manuscript đánh giá rủi ro sinh cảnh</a></p>
+            <p style="text-align: center;"><iframe style="width: 90vw; height: 80vh;" src="https://docs.google.com/document/d/e/2PACX-1vSF5NLSIgZuyc8IUpKQLWG9zFJe3RSK0UuJRnV6ZuY2UBRMEfdZmju2MnRR2mn8EQ/pub?embedded=true"></iframe></p>
         </div>
     </div>
 `; 
