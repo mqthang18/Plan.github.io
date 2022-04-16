b681b58ffcdaee1bb515cdb3d434e71aeaad9398
mqthang_18sh <maiquocthangt@gmail.com>
2022-04-16 09:40:43 +0700

    Update

---
+++ src/views/pages/SumDevices/data.js
@@ -0,0 +1,296 @@
+var data = `
+    <div id="Wrapper">
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Khu vực</th>
+                <th>Trạng thái thiết bị</th>
+                <th>Thời gian bắt đầu ghi</th>
+                <th>Số ngày ghi</th>
+                <th>Tọa độ lắp (tọa độ GE)</th>
+                <th>Link download</th>
+                <th>Ghi chú</th>
+            </tr>
+            <tr>
+                <td>MyThanh1</td>
+                <td>Soc Trang</td>
+                <td>Bình thường</td>
+                <td>16/02/2022 0:00</td>
+                <td>24</td>
+                <td>9.4204 106.17794</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/10hA76ct2mIY7LfUwdDAKrQUB53ZtXxYF/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>MyThanh2</td>
+                <td>Soc Trang</td>
+                <td>Bình thường</td>
+                <td>16/02/2022 0:00</td>
+                <td>24</td>
+                <td>9.42036 106.18051</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1se-Y8wiHSbKsXV-9s-KLQSpUESVVfNba/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>MyThanh3</td>
+                <td>Soc Trang</td>
+                <td>Bình thường</td>
+                <td>16/02/2022 0:00</td>
+                <td>24</td>
+                <td>9.42031 106.18263</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/11JwSXDNTqlMTw-LKusKm768Zn1chfD8Q/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>MyThanh4</td>
+                <td>Soc Trang</td>
+                <td>Bình thường</td>
+                <td>16/02/2022 0:00</td>
+                <td>24</td>
+                <td>9.4201 106.18417</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1vct1jA0EYi_wfUCi1cG6ytxA6lNNjPtT/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>MyThanh5</td>
+                <td>Soc Trang</td>
+                <td>Bình thường</td>
+                <td>16/02/2022 0:00</td>
+                <td>24</td>
+                <td>9.41844 106.18568</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1WmxCkYP6bCoMG4Fzr-uAcQ8J91vuwPJG/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>MyThanh6</td>
+                <td>Soc Trang</td>
+                <td>Bình thường</td>
+                <td>16/02/2022 0:00</td>
+                <td>24</td>
+                <td>9.417 106.1864</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1oLecfA9Wf6atrVGnc2KRbZc_3aB2VrrA/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>MyThanh7</td>
+                <td>Soc Trang</td>
+                <td>Bình thường</td>
+                <td>16/02/2022 0:00</td>
+                <td>24</td>
+                <td>9.41651 106.18561</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1oO-gjr6tdnnlAXnKRUuPkGiCN3uPyNkL/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>MyThanh8</td>
+                <td>Soc Trang</td>
+                <td>Bình thường</td>
+                <td>16/02/2022 0:00</td>
+                <td>24</td>
+                <td>9.41751 106.18451</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/11caHe_M2RnoH-GsvIv6qpw2jMmLP3626/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>MyThanh9</td>
+                <td>Soc Trang</td>
+                <td>Mất thiết bị</td>
+                <td></td>
+                <td></td>
+                <td>9.41844 106.18345</td>
+                <td> </td>
+                <td> </td>
+            </tr>
+
+
+
+            <tr>
+                <td>CM01</td>
+                <td>Ca Mau</td>
+                <td>Bình thường</td>
+                <td>15/01/2022 0:00</td>
+                <td>24</td>
+                <td>8.6478 104.71547</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/11_nQxpJiZczHrEpQH4liU0obb4tJBNa5/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>CM02</td>
+                <td>Ca Mau</td>
+                <td>Bình thường</td>
+                <td>15/01/2022 0:00</td>
+                <td>24</td>
+                <td>8.64607 104.71452</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1gGQXbk8FVgboul2PpdDRmXj5XrLCe56J/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>CM03</td>
+                <td>Ca Mau</td>
+                <td>Mất thiết bị</td>
+                <td></td>
+                <td></td>
+                <td>8.64406 104.71363</td>
+                <td> </td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>CM04</td>
+                <td>Ca Mau</td>
+                <td>Bình thường</td>
+                <td>15/01/2022 0:00</td>
+                <td>24</td>
+                <td>8.64041 104.7126</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1kaUb-gtePOzaB6U2UjnUL1KoFQHZr9Nd/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>CM05</td>
+                <td>Ca Mau</td>
+                <td>Bình thường</td>
+                <td>15/01/2022 0:00</td>
+                <td>24</td>
+                <td>8.63867 104.71243</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1rghSzhUrAFFf4n7BI1pneLFNO-cqFVe-/view">Download</a></td>
+                <td>Bị nước biển vô</td>
+            </tr>
+            <tr>
+                <td>CM06</td>
+                <td>Ca Mau</td>
+                <td>Bình thường</td>
+                <td>15/01/2022 0:00</td>
+                <td>24</td>
+                <td>8.63686 104.71247</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1rxrWA4lqCmMdmilEsmx-cXwMIar6K9Sm/view">Download</a></td>
+                <td>Bị nước biển vô</td>
+            </tr>
+            <tr>
+                <td>CM07</td>
+                <td>Ca Mau</td>
+                <td>Mất thiết bị</td>
+                <td></td>
+                <td></td>
+                <td>8.64406 104.71363</td>
+                <td> </td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>CM08</td>
+                <td>Ca Mau</td>
+                <td>Bình thường</td>
+                <td>15/01/2022 0:00</td>
+                <td>16</td>
+                <td>8.63925 104.71179</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1nbxakZ8e0EQ_cOYLZRpQKeAI5j9S5HaV/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>CM09</td>
+                <td>Ca Mau</td>
+                <td>Bình thường</td>
+                <td>15/01/2022 0:00</td>
+                <td>24</td>
+                <td>8.63716 104.71165</td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/16ZpTNyRlpgkw4hIIgYe6cgbRl8bbgH_n/view">Download</a></td>
+                <td> </td>
+            </tr>
+
+
+            <tr>
+                <td>KG_1</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1ug8U1ShnPUdl97EvKFOugO7z0U5VSp9-/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>KG_2</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1Ec3xHEpce6Q4uwLLPJcAVrSTThWbqdqK/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>KG_3</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1VH-dI97-ts1lCFsspHQMmXATYR7ys_UF/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>KG_4</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1jLWDXtLmsolUOZkatbw_WMTAqFXFD2m0/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>KG_5</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1nZeDblUrqWVygp-BQqAQHQggDpN161Vk/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>KG_6</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1OPQagNLx0AUpLmLMFZjNsFI-GiGVlLvc/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>KG_7</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/18IW3YQlERwEBufdS8l77nDKac7Ez8Rw9/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>KG_8</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1hjOEavve4JUPh94lBOBo7Tu5VtbZ-pB4/view">Download</a></td>
+                <td> </td>
+            </tr>
+            <tr>
+                <td>KG_9</td>
+                <td>Kien Giang</td>
+                <td>Bình thường</td>
+                <td>10/02/2022 12:00</td>
+                <td>24</td>
+                <td> </td>
+                <td><a target="_blank" href="https://drive.google.com/file/d/1xLepQnt6kKrMHf6ZShYg3jL2eH7z7JtE/view">Download</a></td>
+                <td> </td>
+            </tr>
+        </table>
+
+        <p>Ghi chú: 
+        <br>
+        Tọa độ cắm thiết bị tại Kiên Giang vẫn còn đang chờ bác Giang kiểm tra lại.
+        </p>
+    </div>
+`;
---
+++ src/views/pages/SumDevices/header.js
@@ -0,0 +1,5 @@
+var header = `
+    <a href="index.html">Quay về trang chủ</a>
+    <br>
+    <h1>Tóm tắt tình hình thiết bị</h1>
+`; 
